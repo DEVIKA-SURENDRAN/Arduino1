5e045756f5caecf390504fd55d06602bf699e683
DEVIKA-SURENDRAN <devikasurendran309@gmail.com>
2024-06-25 09:08:18 +0530

    Add files via upload
    basic arduino program

---
+++ powerful_uusam_blad1.ino
@@ -0,0 +1,28 @@
+// C++ code
+//
+int led1= 13;
+int led2= 7;
+int led3=4;
+  void setup()
+  
+{
+  pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
+    pinMode(led3, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(led1, HIGH);
+  delay(3000); // Wait for 1000 millisecond(s)
+  digitalWrite(led1, LOW);
+
+  digitalWrite(led2, HIGH);
+  delay(3000); // Wait for 1000 millisecond(s)
+  digitalWrite(led2, LOW);
+  
+  digitalWrite(led3, HIGH);
+  delay(3000); // Wait for 1000 millisecond(s)
+  digitalWrite(led3, LOW);
+ 
+} 
